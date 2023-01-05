e8c223bec8c89a556dc40d6c351540c511abaa79
Имя <vitya.klyucherov@mail.ru>
2023-01-05 18:15:57 +0300

    Дз. Объектно-ориентированное программирование. Часть 1

--- src/Car.java
+++ src/Car.java
@@ -1,1 +1,66 @@
+public class Car {
+
+    String brand;
+    String model;
+    double engineVolume;
+    String color;
+    int year;
+    String country;
+
+    public Car(String brand, String model, double engineVolume, String color, int year, String country) {
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.year = year;
+        this.country = country;
+    }
+
+    Car() {
+        this("default ", "default", 1.5, "Белый", 2000, "default");
+
+    }
+
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getEngineVolume() {
+        return engineVolume;
+    }
+
+    public void setEngineVolume(int engineVolume) {
+        this.engineVolume = engineVolume;
+    }
+
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "Автомобиль " + brand + model + "," + " Год выпуска " + year + "," + " Страна изготовитель " + country + ", " + color + " цвета" + ", " + "Объём двигателя " + engineVolume + " л";
+    }
+}
+
 
